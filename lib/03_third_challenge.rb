e1027a6a6c7e13901b2173a05510f86a13b2f2b8
Matthew Kindt <matthewkindt@yahoo.com>
2020-09-10 18:48:03 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -13,6 +13,7 @@ def third_challenge
         :name => "Romeo"
         },
         { :age => "15" },
+        { :status => "alive" },
       :hero_friends => []
    }, 
    :capulet => {
@@ -25,8 +26,10 @@ def third_challenge
         },
         { :age => "51" },
       :heroine => {
-        
+        :name => "Juliet"
         },
+        { :age => "15" },
+        { :status => "alive"},
       :heroine_friends => []
    }
   }
